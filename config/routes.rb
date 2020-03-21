b9c45af68b90ccc23328271bb817dcc24fc83745
vincentbwalker <vincethewalker@gmail.com>
2020-03-21 20:04:30 +0000

    Completed devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  root to: 'books#index'
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  
   
   
   resources :books
